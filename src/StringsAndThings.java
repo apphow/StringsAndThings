24aeaa03444745f36beece32235cc540cc02cb59
aprilhoward <apehow111@gmail.com>
2020-10-10 18:00:15 -0400

    Initial commit

---
+++ src/StringsAndThings.java
@@ -0,0 +1,26 @@
+public class StringsAndThings {
+
+    /**
+     * Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count,
+     * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
+     * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
+     * example : countYZ("fez day"); // Should return 2
+     * countYZ("day fez"); // Should return 2
+     * countYZ("day fyyyz"); // Should return 2
+     */
+
+    public static int countYZ(String input) {
+        String[] arr = input.split(" ");
+        int count = 0;
+        for (String each : arr) {
+            if (each.charAt(each.length() - 1) == 'y' || each.charAt(each.length() - 1) == 'z') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String removeString(String base, String remove) {
+        return base.replaceAll(remove, "");
+    }
+}
---
+++ test/StringsAndThingsTest.java
@@ -0,0 +1,52 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringsAndThingsTest {
+
+    private StringsAndThings stringsAndThings;
+
+    @Before
+    public void setUp() throws Exception {
+
+        stringsAndThings = new StringsAndThings();
+    }
+
+    @Test
+    public void removeString() {
+    }
+
+    @Test
+    public void countYZ() {
+        String input = "fez day";
+        int expected = 2;
+        int actual = stringsAndThings.countYZ(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countYZ2() {
+        String input = "yesterday was such a lovely day";
+        int expected = 3;
+        int actual = StringsAndThings.countYZ(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countYZ3() {
+        String input = "z y z y z zebra";
+        int expected = 5;
+        int actual = StringsAndThings.countYZ(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countYZ4() {
+        String input = "fizz buzz fuzzy buzzy";
+        int actual = 4;
+        int expected = StringsAndThings.countYZ(input);
+        Assert.assertEquals(expected, actual);
+    }
+}
